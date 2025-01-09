d3542428632257d0dd9ad9bd9d3d1209b95b5c74
Lizandro Melo <lizandro.melllo@gmail.com>
2025-01-09 09:00:00 -0300

    Adiciona componente MainContant à página de login e estrutura básica do layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
-
+import MainContant from "@/components/container/main-contant";
 
 
 export default function Login() {
-  return (
-    <>
+    return (
+        <MainContant>
+            <main className="bg-white w-full h-full">
 
-    </>
-  );
+            </main>
+        </MainContant>
+
+    );
 }
